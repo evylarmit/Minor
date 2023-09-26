4b785d62c84b8d4edb9a5597ad703da649427d26
evylarmit <122461492+evylarmit@users.noreply.github.com>
2023-09-26 08:56:10 +0000

    Basis

---
+++ weatherapi.js
@@ -0,0 +1,41 @@
+function getWeatherData(location) {
+    const apiKey = "5d250a986f6336676e5b65f41f2d381a";
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${apiKey}`;
+    return fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        const weatherData = {
+          temperature: data.main.temp,
+          condition: data.weather[0].main,
+          location: data.name,
+        };
+        return weatherData;
+      });
+  }
+  function updateUI(weatherData) {
+    const temperature = document.querySelector("#temperature");
+    const condition = document.querySelector("#condition");
+    const location = document.querySelector("#location");
+  
+    temperature.textContent = `${weatherData.temperature}°C`;
+    condition.textContent = weatherData.condition;
+    location.textContent = weatherData.location;
+    
+  }
+  
+  const searchBtn = document.querySelector("#search-btn");
+  const searchBar = document.querySelector("#search-bar");
+  
+  searchBtn.addEventListener("click", () => {
+    const location = searchBar.value;
+    
+    getWeatherData(location)
+      .then(weatherData => {
+        updateUI(weatherData);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  });
+  
+  
