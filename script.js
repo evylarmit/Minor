91ebd62fa5744b6a01d8ac2a99b078e187bdfc30
evylarmit <122461492+evylarmit@users.noreply.github.com>
2023-09-26 12:12:27 +0000

    Pop-up start

--- script.js
+++ script.js
@@ -5,3 +5,18 @@ function openNav() {
   function closeNav() {
     document.getElementById("myNav").style.width = "0%";
   }
+
+function flipflop() {
+  if (document.getElementById("search-bar").style.display === "none") {
+    document.getElementById("search-bar").style.display = "block";
+    document.getElementById("weather-applicatie").style.display = "none";
+  } else {
+    document.getElementById("search-bar").style.display = "none";
+    document.getElementById("weather-applicatie").style.display = "block";
+  }
+}
+
+function popUp() {
+  let popup = document.getElementById("myPopup");
+  popup.classList.toggle("show");
+}
